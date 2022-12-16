c10c357827ec6b48fb08720c6efd2f46bfa3fca6
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2022-12-16 08:47:23 -0800

    initial file and directory setup

---
+++ PierresVendorTracker/Controllers/OrdersController.cs
@@ -0,0 +1,12 @@
+// using System.Collections.Generic;
+// using System;
+using Microsoft.AspNetCore.Mvc;
+using PierresVendors.Models;
+
+namespace PierresOrders.Controllers
+{
+  public class OrdersController : Controller
+  {
+
+  }
+}
