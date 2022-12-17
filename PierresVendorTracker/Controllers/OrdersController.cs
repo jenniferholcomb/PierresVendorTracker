b6eafed0e1e95956a6981b1f53a837294ee26e40
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2022-12-16 17:03:10 -0800

    new page with form to add order to vendor

--- PierresVendorTracker/Controllers/OrdersController.cs
+++ PierresVendorTracker/Controllers/OrdersController.cs
@@ -1,4 +1,4 @@
-// using System.Collections.Generic;
+using System.Collections.Generic;
 // using System;
 using Microsoft.AspNetCore.Mvc;
 using PierresVendors.Models;
@@ -7,6 +7,18 @@ namespace PierresOrders.Controllers
 {
   public class OrdersController : Controller
   {
+    [HttpGet("/orders")]
+    public ActionResult Index()
+    {
+      List<Order> allOrders = Order.GetAll();
+      return View(allOrders);
+    }
 
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
   }
 }
